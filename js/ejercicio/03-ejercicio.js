927cdd4051fb669675c0c3f254888f479b63769b
Miguel Angel Ramos <return.infox@gmail.com>
2024-04-01 10:33:43 -0300

    calculo de temperaturas

---
+++ js/ejercicio/03-ejercicio.js
@@ -0,0 +1,8 @@
+//* Grados Fahrenheit = (grados centígrados × 9/5) + 32
+
+alert("Bienvenido(a) al calculo de temperaturas: ")
+alert("Vamos a calcular de grados celsius a fahrenheit")
+
+let celsius = prompt("Ingrese los grados celsius")
+console.log(celsius);
+console.log(typeof celsius); // typeof pregunto el tipo de la variable
