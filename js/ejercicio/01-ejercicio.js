53aac24c48964c60acbb350691d523244b5d511e
Miguel Angel Ramos <return.infox@gmail.com>
2024-04-01 09:31:16 -0300

    ejercicio

---
+++ js/ejercicio/01-ejercicio.js
@@ -0,0 +1,7 @@
+/**
+ * Se pide crear un control de acceso al cine para ver una pelicula que requiere se tenga 18 años o mas, para poder verla.
+ */
+
+alert("Bienvenido(a) al Cine.");
+let edad = prompt("Cual es tu edad ? :");
+console.log(edad);
