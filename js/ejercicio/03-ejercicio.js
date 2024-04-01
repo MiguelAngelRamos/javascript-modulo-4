caf646f072c3b1e98a6f8d76cbb7c377a50c969d
Miguel Angel Ramos <return.infox@gmail.com>
2024-04-01 11:26:54 -0300

    operadores de comparacion

--- js/ejercicio/03-ejercicio.js
+++ js/ejercicio/03-ejercicio.js
@@ -3,6 +3,12 @@
 alert("Bienvenido(a) al calculo de temperaturas: ")
 alert("Vamos a calcular de grados celsius a fahrenheit")
 
-let celsius = prompt("Ingrese los grados celsius")
-console.log(celsius);
-console.log(typeof celsius); // typeof pregunto el tipo de la variable
+let celsius = parseFloat(prompt("Ingrese los grados celsius"));
+
+// 10.2 -> es float  tiene punto decimal o punto flotante
+// 34.6 -> es float tiene punto decimal o punto flotante
+
+let gradosFahrenheit = (celsius * 9/5) + 32;
+
+// concatenación
+console.log(celsius + "ºC es igual a " + gradosFahrenheit + "ºF");
---
+++ js/variables/apuntes.md
@@ -0,0 +1,14 @@
+## Operadores de Comparación
+
+1. igual (==), comprueba si dos valores son iguales.
+
+```sh
+ '10' == 5 // true
+```
+2. Estrictamente igual (===) compara valor y tipo
+
+Comprueba si dos valores son iguales y son del mismo tipo
+
+```sh
+  '10' === 5 // false
+```
