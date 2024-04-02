8975fe4eb3dafc33f17666b01076c50fb908deb0
Miguel Angel Ramos <return.infox@gmail.com>
2024-04-02 12:05:04 -0300

    actividad grupal 1

--- js/ejercicio/03-ejercicio.js
+++ js/ejercicio/03-ejercicio.js
@@ -10,5 +10,11 @@ let celsius = parseFloat(prompt("Ingrese los grados celsius"));
 
 let gradosFahrenheit = (celsius * 9/5) + 32;
 
+//* Calcule de grados Celsius a Kelvin
+
+//* aqui haga su calculo.... K = ºC + 273.15.
+
 // concatenación
-console.log(celsius + "ºC es igual a " + gradosFahrenheit + "ºF");
+console.log(celsius + "ºC es igual a " + gradosFahrenheit + "ºF" + ", celsius con convertidos a kelvin son: " +  kelvin);
+
+//* HAGA EL DIAGRAMA DE FLUJO DE ESTE ALGORITMO 11:20 envie su captura en el chat
---
+++ js/ejercicio/06-ejercicio.js
@@ -0,0 +1,15 @@
+//* Voy a sumar 2 numeros y voy a calcular el promedio
+
+let numero1 = parseInt(prompt("Ingrese el numero: 1: "));
+let numero2 = parseInt(prompt("Ingrese el numero: 2: "));
+let numero3 = parseInt(prompt("Ingrese el numero: 3: "));
+
+//* Calcular la sumatoria
+
+let suma = numero1 + numero2 + numero3;
+
+let promedio = suma/3;
+
+//* Presento la información
+console.log("La suma de los 2 numeros es: " +  suma);
+console.log("El promedio de los 2 numeros es: " +  promedio);
